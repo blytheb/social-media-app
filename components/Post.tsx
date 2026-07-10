bdeca3a17383e60403995b4367b1a66c1228f5dc
blytheb <blytheballesteros@gmail.com>
2026-07-10 12:58:30 -1000

    added responsiveness for post text

--- components/Post.tsx
+++ components/Post.tsx
@@ -22,8 +22,17 @@ export function PostHeader() {
 
 			<div className="text-[15px] flex flex-col space-y-1.5">
 				<div className="flex space-x-1.5 text-[#707E89]">
-					<span className="font-bold text-[#0F1419]">Guest</span>
-					<span>@guest000234</span>
+					<span
+						className="font-bold text-[#0F1419]
+                     inline-block whitespace-nowrap overflow-hidden text-ellipsis max-w-[60px] min-[400px]:max-w-[100px] min-[500px]:max-w-[140px]
+                     sm:max-w-[160px]">
+						Guasdfasdfasdfasdfest
+					</span>
+					<span
+						className="inline-block whitespace-nowrap overflow-hidden text-ellipsis max-w-[60px] min-[400px]:max-w-[100px] min-[500px]:max-w-[140px]
+                     sm:max-w-[160px]">
+						@guest000234
+					</span>
 					<span>.</span>
 					<span>a day ago</span>
 				</div>
