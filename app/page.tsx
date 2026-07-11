516be5e1512b49d61ca7cb28c5b24ac8b65f0e8c
blytheb <blytheballesteros@gmail.com>
2026-07-10 14:15:51 -1000

    completed signup bottom banner prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import PostFeed from "@/components/PostFeed";
 import Sidebar from "@/components/Sidebar";
+import SignUpPrompt from "@/components/SignUpPrompt";
 import Widgets from "@/components/Widgets";
 import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="text-[#0F1419] min-h-screen max-w-[1400px]  mx-auto flex justify-center">
-			<Sidebar />
-			<PostFeed />
-			<Widgets />
-		</div>
+		<>
+			<div className="text-[#0F1419] min-h-screen max-w-[1400px]  mx-auto flex justify-center">
+				<Sidebar />
+				<PostFeed />
+				<Widgets />
+			</div>
+			<SignUpPrompt />
+		</>
 	);
 }
