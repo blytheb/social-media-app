9ce05ce03c8205b9513f95ebd0233c6b62825cbe
blytheb <blytheballesteros@gmail.com>
2026-07-13 13:50:30 -1000

    access sign up modal open state in modal compt using redux store

--- components/modals/SignUpModal.tsx
+++ components/modals/SignUpModal.tsx
@@ -2,28 +2,37 @@
 
 import React, { useState } from "react";
 import { Modal } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 export default function SignUpModal() {
-	const [isOpen, setIsOpen] = useState(false);
+	// const [isOpen, setIsOpen] = useState(false);
 
-	const handleClose = () => {
-		setIsOpen(false);
-	};
+	// const handleClose = () => {
+	// 	setIsOpen(false);
+	// };
 
-	const handleOpen = () => {
-		setIsOpen(true);
-	};
+	// const handleOpen = () => {
+	// 	setIsOpen(true);
+	// };
+
+	const isOpen = useSelector(
+		(state: RootState) => state.modals.signUpModalOpen,
+	);
+	console.log(isOpen);
+
 	return (
 		<>
 			<button
 				className="w-full h-[48px] md:w-[88px] md:h-[40px] text-md md:text-sm font-bold bg-white rounded-full"
-				onClick={handleOpen}>
+				// onClick={handleOpen}
+			>
 				Sign Up
 			</button>
 
 			<Modal
 				open={isOpen}
-				onClose={handleClose}
+				// onClose={handleClose}
 				className="flex justify-center items-center">
 				<div className="w-[200px] h-[400px] bg-white"></div>
 			</Modal>
