39d394960463dca3916bfbc50b281a42f488c198
blytheb <blytheballesteros@gmail.com>
2026-07-13 14:03:10 -1000

    use dispatch to make modal access global

--- components/modals/SignUpModal.tsx
+++ components/modals/SignUpModal.tsx
@@ -2,37 +2,29 @@
 
 import React, { useState } from "react";
 import { Modal } from "@mui/material";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { closeSignUpModal, openSignUpModal } from "@/redux/slices/modalSlice";
 
 export default function SignUpModal() {
-	// const [isOpen, setIsOpen] = useState(false);
-
-	// const handleClose = () => {
-	// 	setIsOpen(false);
-	// };
-
-	// const handleOpen = () => {
-	// 	setIsOpen(true);
-	// };
-
 	const isOpen = useSelector(
 		(state: RootState) => state.modals.signUpModalOpen,
 	);
+
+	const dispatch: AppDispatch = useDispatch();
 	console.log(isOpen);
 
 	return (
 		<>
 			<button
 				className="w-full h-[48px] md:w-[88px] md:h-[40px] text-md md:text-sm font-bold bg-white rounded-full"
-				// onClick={handleOpen}
-			>
+				onClick={() => dispatch(openSignUpModal())}>
 				Sign Up
 			</button>
 
 			<Modal
 				open={isOpen}
-				// onClose={handleClose}
+				onClose={() => dispatch(closeSignUpModal())}
 				className="flex justify-center items-center">
 				<div className="w-[200px] h-[400px] bg-white"></div>
 			</Modal>
