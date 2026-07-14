cddfe001406d77bee11c890c895092547b3bd30f
blytheb <blytheballesteros@gmail.com>
2026-07-14 13:36:30 -1000

    fix: modal stays closed on log out, long names get cut short for userinfo sidebar

--- components/SidebarUserInfo.tsx
+++ components/SidebarUserInfo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { auth } from "@/firebase";
+import { closeLogInModal, closeSignUpModal } from "@/redux/slices/modalSlice";
 import { signOutUser } from "@/redux/slices/userSlice";
 import { AppDispatch, RootState } from "@/redux/store";
 import { signOut } from "firebase/auth";
@@ -17,10 +18,14 @@ export default function SidebarUserInfo() {
 	async function handleSignOut() {
 		await signOut(auth);
 		dispatch(signOutUser());
+
+		dispatch(closeSignUpModal());
+		dispatch(closeLogInModal());
 	}
 	return (
 		<div
-			className="absolute bottom-3 flex items-center space-x-2 xl:p-3 xl:pe-6 rounded-full hover:bg-gray-500 hover:bg-opacity-10 transition cursor-pointer"
+			className="absolute bottom-3 flex items-center space-x-2 xl:p-3 xl:pe-6 rounded-full
+			 hover:bg-gray-500 hover:bg-opacity-10 transition cursor-pointer w-fit xl:w-[240px]"
 			onClick={() => handleSignOut()}>
 			<Image
 				src={"/assets/profile-pic.png"}
@@ -30,9 +35,13 @@ export default function SidebarUserInfo() {
 				className="w-9 h-9"
 			/>
 
-			<div className="hidden xl:flex flex-col text-sm ">
-				<span className="font-bold ">{user.name}</span>
-				<span className="text-gray-500">@{user.username}</span>
+			<div className="hidden xl:flex flex-col text-sm max-w-40">
+				<span className=" whitespace-nowrap text-ellipsis overflow-hidden font-bold ">
+					{user.name}
+				</span>
+				<span className=" whitespace-nowrap text-ellipsis overflow-hidden text-gray-500">
+					@{user.username}
+				</span>
 			</div>
 		</div>
 	);
